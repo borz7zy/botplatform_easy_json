d3581c021a84377fbcf16644955a8382adde279f
borz7zy <borz7zy@yandex.ru>
2024-09-20 18:08:53 +0200

    Initial commit

---
+++ easy_json.cpp
@@ -0,0 +1,160 @@
+#include <iostream>
+#include <fstream>
+#include <json.hpp>
+#include <sstream>
+#include <easy_json.hxx>
+
+using json = nlohmann::json;
+
+namespace local_easy_json_functions
+{
+    std::vector<std::string> split(const std::string &str, char delimiter)
+    {
+        std::vector<std::string> tokens;
+        std::stringstream ss(str);
+        std::string token;
+        while (std::getline(ss, token, delimiter))
+        {
+            tokens.push_back(token);
+        }
+        return tokens;
+    }
+};
+
+namespace easy_json
+{
+
+    int file_get_value_string(const char *file_path, const char *key_path, std::string &value)
+    {
+        std::ifstream file(file_path);
+        if (!file.is_open())
+        {
+            return ERROR_LOADING_FILE;
+        }
+
+        json j;
+        try
+        {
+            file >> j;
+        }
+        catch (const json::parse_error &e)
+        {
+            return ERROR_PARSING;
+        }
+
+        std::vector<std::string> keys = local_easy_json_functions::split(key_path, '.');
+        json current = j;
+        for (const std::string &key : keys)
+        {
+            if (current.contains(key))
+            {
+                current = current[key];
+            }
+            else
+            {
+                return ERROR_KEY_PATH;
+            }
+        }
+
+        try
+        {
+            value = current.get<std::string>();
+        }
+        catch (const json::type_error &e)
+        {
+            return ERROR_INVALID_TYPE_DATA;
+        }
+
+        return NO_ERROR;
+    }
+
+    // Получение целочисленного значения
+    int file_get_value_int(const char *file_path, const char *key_path, int &value)
+    {
+        std::ifstream file(file_path);
+        if (!file.is_open())
+        {
+            return ERROR_LOADING_FILE;
+        }
+
+        json j;
+        try
+        {
+            file >> j;
+        }
+        catch (const json::parse_error &e)
+        {
+            return ERROR_PARSING;
+        }
+
+        std::vector<std::string> keys = local_easy_json_functions::split(key_path, '.');
+        json current = j;
+        for (const std::string &key : keys)
+        {
+            if (current.contains(key))
+            {
+                current = current[key];
+            }
+            else
+            {
+                return ERROR_KEY_PATH;
+            }
+        }
+
+        try
+        {
+            value = current.get<int>();
+        }
+        catch (const json::type_error &e)
+        {
+            return ERROR_INVALID_TYPE_DATA;
+        }
+
+        return NO_ERROR;
+    }
+
+    int file_get_value_bool(const char *file_path, const char *key_path, bool &value)
+    {
+        std::ifstream file(file_path);
+        if (!file.is_open())
+        {
+            return ERROR_LOADING_FILE;
+        }
+
+        json j;
+        try
+        {
+            file >> j;
+        }
+        catch (const json::parse_error &e)
+        {
+            return ERROR_PARSING;
+        }
+
+        std::vector<std::string> keys = local_easy_json_functions::split(key_path, '.');
+        json current = j;
+        for (const std::string &key : keys)
+        {
+            if (current.contains(key))
+            {
+                current = current[key];
+            }
+            else
+            {
+                return ERROR_KEY_PATH;
+            }
+        }
+
+        try
+        {
+            value = current.get<bool>();
+        }
+        catch (const json::type_error &e)
+        {
+            return ERROR_INVALID_TYPE_DATA;
+        }
+
+        return NO_ERROR;
+    }
+
+};
---
+++ easy_json.hxx
@@ -0,0 +1,28 @@
+#if defined(_WIN32) || defined(_WIN64)
+#define LIB_EXPORT __declspec(dllexport)
+#elif defined(__linux__) || defined(__APPLE__)
+#define LIB_EXPORT __attribute__((visibility("default")))
+#else
+#define LIB_EXPORT
+#endif
+
+namespace easy_json
+{
+    enum Errors
+    {
+        ERROR_LOADING_FILE = 1,
+        ERROR_PARSING,
+        ERROR_KEY_PATH,
+        ERROR_INVALID_TYPE_DATA,
+
+        NO_ERROR = 0
+    };
+
+    extern "C"
+    {
+        LIB_EXPORT int file_get_value_string(const char *file_path, const char *key_path, char *value, int value_size);
+        LIB_EXPORT int file_get_value_int(const char *file_path, const char *key_path, int *value);
+        LIB_EXPORT int file_get_value_bool(const char *file_path, const char *key_path, bool *value);
+    }
+
+}
